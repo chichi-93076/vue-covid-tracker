40e457f7647160cc4878c5e6de583de937ce9abe
chichi <93076@gap.wzu.edu.tw>
2022-04-12 10:19:00 +0800

    20220412 learn vue & tailwind

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <select @change="onChange()"
+    v-model="selectd" 
+    class="form-select mt-10 block w-full border p-3 rounded">
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :key="country.ID" :value="country.ID">
+      {{ country.Country }}
+    </option> 
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'CountrySelect',
+  props: ['countries'],
+  data() {
+    return {
+      selectd: 0
+    }
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selectd)
+
+      console.log(country)
+      
+      this.$emit('get-country', country)
+    },
+  },
+}
+</script>
